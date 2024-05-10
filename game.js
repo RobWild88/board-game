6c65d9f42065f3eb020bf88a651f67e9e47360cf
RobWild88 <robwild88@yahoo.com>
2024-05-10 06:46:01 +0000

    Added Start Position and console messages for moving pieces

--- game.js
+++ game.js
@@ -1,1 +1,25 @@
-var board1 = Chessboard('board1', 'start')
+function logic(
+  source, target, piece, newPosition, oldPosition, orientation
+){
+  console.log("source", source)
+  console.log("target", target)
+  console.log("piece", piece)
+  console.log("newPosition", newPosition)
+  console.log("oldPosition", oldPosition)
+  console.log("orientation", orientation)
+  console.log("------------")
+  //return "snapback"
+}
+
+
+
+let board2 = Chessboard('board2', {
+  draggable: true,
+  dropOffBoard: 'trash',
+  sparePieces: true,
+  onDrop: logic
+})
+
+$('#startBtn').on('click', board2.start)
+$('#clearBtn').on('click', board2.clear)
+board2.start()
